01c157cb08af9be808bfa27e0dfd1bd15615b8a1
Catherine Dieval <catherine.dieval@hotmail.fr>
2024-02-02 12:40:49 +0100

    Made a correction

--- backend-basics_chance/server.js
+++ backend-basics_chance/server.js
@@ -2,11 +2,11 @@ import { createServer } from "node:http";
 import Chance from "chance";
 
 const chance = new Chance();
-let name = chance.name({ gender: "female", nationality: "en" });
-let age = chance.age({ type: "adult" });
-let profession = chance.profession({ rank: true });
 
 export const server = createServer((request, response) => {
+  let name = chance.name({ gender: "female", nationality: "en" });
+  let age = chance.age({ type: "adult" });
+  let profession = chance.profession({ rank: true });
   response.statusCode = 200;
   response.end(
     `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`
