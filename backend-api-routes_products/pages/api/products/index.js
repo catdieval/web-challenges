ce6e0cf9a3fc2e054349b1362ec7af2afa669367
Catherine <catherine.dieval@hotmail.fr>
2024-08-08 19:30:07 +0200

    Corrected index.js in api/products folder

--- backend-api-routes_products/pages/api/products/index.js
+++ backend-api-routes_products/pages/api/products/index.js
@@ -1,8 +1,11 @@
 import { getAllProducts } from "/services/productServices.js";
 
+const products = getAllProducts();
+
 export default function handler(request, response) {
   if (request.method === "GET") {
-    response.status(200).json(getAllProducts());
+    //response.status(200).json(getAllProducts());
+    response.status(200).json(products);
     return;
   }
 }
