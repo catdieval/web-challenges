928bec30d61a9f2c6c80629f9fe051c4d8635d02
Catherine <catherine.dieval@hotmail.fr>
2024-08-09 11:19:55 +0200

    Corrected index.js in api/products

--- backend-api-routes_products/pages/api/products/index.js
+++ backend-api-routes_products/pages/api/products/index.js
@@ -4,7 +4,6 @@ const products = getAllProducts();
 
 export default function handler(request, response) {
   if (request.method === "GET") {
-    //response.status(200).json(getAllProducts());
     response.status(200).json(products);
     return;
   }
